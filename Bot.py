08bf91a275a90ed6fa3849caa9a891f5327a912c
Иван Доронин <bucefalfun@yandex.ru>
2019-08-26 01:53:02 +0300

    'initial'

--- Bot.py
+++ Bot.py
@@ -4,36 +4,10 @@
 import telebot
 from telebot import apihelper
 import requests
-import cv2 as cv
+#from cv2 import as cv
 
 API_TOKEN = "977146141:AAECBdZgzXroKRLr7TKfRgLveext4bbp-cw"
 apihelper.proxy = {'http': 'http://10.10.1.10:3128'}
-
-
-def Vpaste(background, obj, msk="white", plc="l_d", part=0.3):
-    s = min(obj.shape[0] / background.shape[0], obj.shape[1] / background.shape[1])
-    ob = cv.resize(obj, None, fx=part / s, fy=part / s, interpolation=cv.INTER_AREA)
-    gray = cv.cvtColor(ob, cv.COLOR_BGR2GRAY)
-    if msk == "white":
-        ret, mask = cv.threshold(gray, gray.max() - 25, gray.max(), cv.THRESH_BINARY)
-        mask = cv.bitwise_not(mask)
-    else:
-        ret, mask = cv.threshold(gray, gray.min(), gray.max(), cv.THRESH_BINARY)
-    mask_inv = cv.bitwise_not(mask)
-    n, m, l = ob.shape
-    # corner selection
-    corner = plc.split("_")
-    a, b, c, d = 0, n, 0, m
-    if corner[0] == "r":
-        c, d = -m - 1, -1
-    if corner[1] == "d":
-        a, b = -n - 1, -1
-    roi = background[a:b, c:d]
-    m1 = cv.bitwise_and(roi, roi, mask=mask_inv)
-    m2 = cv.bitwise_and(ob, ob, mask=mask)
-    dst = cv.add(m1, m2)
-    background[a:b, c:d] = dst
-    return background
 
 
 bot = telebot.TeleBot(API_TOKEN)
@@ -45,13 +19,39 @@ def get_commands(message):
         bot.send_message(message.from_user.id, "Пришли фото фона")
 
 
-# @bot.message_handler(content_types=['photo'])
-# def get_back(message):
-#    file_info = bot.get_file()
+# def Vpaste(background, obj, msk="white", plc="l_d", part=0.3):
+#     s = min(obj.shape[0] / background.shape[0], obj.shape[1] / background.shape[1])
+#     ob = cv.resize(obj, None, fx=part / s, fy=part / s, interpolation=cv.INTER_AREA)
+#     gray = cv.cvtColor(ob, cv.COLOR_BGR2GRAY)
+#     if msk == "white":
+#         ret, mask = cv.threshold(gray, gray.max() - 25, gray.max(), cv.THRESH_BINARY)
+#         mask = cv.bitwise_not(mask)
+#     else:
+#         ret, mask = cv.threshold(gray, gray.min(), gray.max(), cv.THRESH_BINARY)
+#     mask_inv = cv.bitwise_not(mask)
+#     n, m, l = ob.shape
+#     # corner selection
+#     corner = plc.split("_")
+#     a, b, c, d = 0, n, 0, m
+#     if corner[0] == "r":
+#         c, d = -m - 1, -1
+#     if corner[1] == "d":
+#         a, b = -n - 1, -1
+#     roi = background[a:b, c:d]
+#     m1 = cv.bitwise_and(roi, roi, mask=mask_inv)
+#     m2 = cv.bitwise_and(ob, ob, mask=mask)
+#     dst = cv.add(m1, m2)
+#     background[a:b, c:d] = dst
+#     return background
+
 
 @bot.message_handler(content_types=['photo'])
 def get_back(message):
-    obj = cv.imread("Andrew.png")
+    file_info = bot.get_file()
+
+@bot.message_handler(content_types=['photo'])
+def get_back(message):
+    # obj = cv.imread("Andrew.png")
     print(message)
     file_id = message.photo[1].file_id
     file_info = bot.get_file(file_id)
@@ -59,12 +59,12 @@ def get_back(message):
     print(URL)
     fi = requests.get(URL, allow_redirects=True)
     open("tmp1.jpg", 'wb').write(fi.content)
-    background = cv.imread("tmp1.jpg")
-    t = Vpaste(background, obj, msk="white", plc="l_d", part=0.45)
-    cv.imwrite("out.jpg", t)
-    out = open("out.jpg", 'rb')
-    bot.send_message(message.from_user.id, "lol")
-    bot.send_photo(message.from_user.id, out)
+    # background = cv.imread("tmp1.jpg")
+    # t = Vpaste(background, obj, msk="white", plc="l_d", part=0.45)
+    # cv.imwrite("out.jpg", t)
+    # out = open("out.jpg", 'rb')
+    # bot.send_message(message.from_user.id, "lol")
+    # bot.send_photo(message.from_user.id, out)
 
 
 @bot.message_handler(content_types=['text'])
